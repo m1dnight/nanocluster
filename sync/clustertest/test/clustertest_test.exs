b1d9d485f26b57262cdb2ed8f0f4a71fe850b811
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-08-15 14:56:56 +0200

    clone instead of copy

--- sync/clustertest/test/clustertest_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ClustertestTest do
-  use ExUnit.Case
-  doctest Clustertest
-
-  test "greets the world" do
-    assert Clustertest.hello() == :world
-  end
-end
--- sync/run.sh
+++ sync/run.sh
@@ -3,16 +3,17 @@
 # Change to the directory where this script is located
 cd "$(dirname "$0")"
 
-cd clustertest
+rm -rf connect-four
+git clone https://github.com/m1dnight/connect-four.git
 
-mix local.hex --force
-mix local.rebar --force
-# which iex
-# which mix
-mix release --overwrite
+# mix local.hex --force
+# mix local.rebar --force
+# mix deps.get
+# mix compile
+# mix release --overwrite
 
-export RELEASE_COOKIE=secret
-
-pkill -f clustertest
-_build/dev/rel/clustertest/bin/clustertest stop
-_build/dev/rel/clustertest/bin/clustertest daemon
+# export RELEASE_DISTRIBUTION=name
+# export RELEASE_COOKIE=secret
+# export RELEASE_NODE="app@$(hostname -f)"
+# pkill -f beam
+# /home/pi/sync/connect_four/_build/dev/rel/connect_four/bin/connect_four daemon
