72f999d6eeeaada187309e9e9895fede5a62581b
Christophe De Troyer <christophe.detroyer@gmail.com>
2026-01-13 16:58:35 +0100

    wip commit

--- sync/run.sh
+++ sync/run.sh
@@ -3,17 +3,17 @@
 # Change to the directory where this script is located
 cd "$(dirname "$0")"
 
-rm -rf connect-four
-git clone https://github.com/m1dnight/connect-four.git
+# rm -rf connect-four
+# git clone https://github.com/m1dnight/connect-four.git
 
-# mix local.hex --force
-# mix local.rebar --force
-# mix deps.get
-# mix compile
-# mix release --overwrite
+mix local.hex --force
+mix local.rebar --force
+mix deps.get
+mix compile
+mix release --overwrite
 
-# export RELEASE_DISTRIBUTION=name
-# export RELEASE_COOKIE=secret
-# export RELEASE_NODE="app@$(hostname -f)"
-# pkill -f beam
-# /home/pi/sync/connect_four/_build/dev/rel/connect_four/bin/connect_four daemon
+export RELEASE_DISTRIBUTION=name
+export RELEASE_COOKIE=secret
+export RELEASE_NODE="app@$(hostname -f)"
+pkill -f beam
+/home/pi/sync/connect-four/_build/dev/rel/connect_four/bin/connect_four daemon
